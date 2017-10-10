c22a7b6963859781be88688e96006abccedf048a
QLubov <romashishka@bk.ru>
2017-10-10 18:23:19 +0300

    commit all

---
+++ Assets/Moving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+  public GameManager gameMgr;
+  static GameObject clickedGameObject = null;
+  public void OnClick()
+  {
+    //add somthing about focus
+    if (clickedGameObject == null)
+    {
+      clickedGameObject = gameObject;
+    }
+    else
+    {
+      gameMgr.MoveElements(gameObject, clickedGameObject);
+      clickedGameObject = null;
+    }
+  }
+}
